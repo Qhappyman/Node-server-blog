2bbf8d93d7a6fe504dbd65626d0a94b4208fe1a3
Qhappyman <510457468@qq.com>
2020-08-18 14:53:12 +0800

    server base congigure

--- bolg-1/bin/www.js
+++ bolg-1/bin/www.js
@@ -1,7 +1,7 @@
 const http = require('http')       //基础配置文件,真正启动服务的文件
 
-const PORT = 8000
+const PORT = 8000                       //基础端口
 const serverHandle = require('../app')
 
-const server = http.createServer(serverHandle)
+const server = http.createServer(serverHandle)   //创建服务
 server.listen(PORT)
